4582c3be0b0acc6bfad7e4e3d711ef5ad657c96a
ManHoff <achoffman91@gmail.com>
2023-02-10 17:12:32 -0500

    added to navbar.js / added styling to nav app.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import './App.css';
+import NavBar from "./components/NavBar";
+
 
 function App() {
-  return <div>testing</div>;
+    return <NavBar />
 }
+
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+export default function NavBar() {
+    return <nav className="navbar">
+        <a href="/" className="name">Anthony Hoffman</a>
+        <ul>
+            <li>
+                <li>
+                <a href="/about">About</a>
+                </li>
+                <li>
+                <a href="/mywork">My Work</a>
+                </li>
+                <li>
+                <a href="/contact">Contact</a>
+                </li>
+            </li>
+        </ul>
+    </nav>
+}
