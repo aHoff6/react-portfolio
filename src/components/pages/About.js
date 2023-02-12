984034404b12b5527517f21c3db52b9f0622b154
ManHoff <achoffman91@gmail.com>
2023-02-11 20:03:18 -0500

    added about section and pics for myWork

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,3 +1,22 @@
 export default function About() {
-    return <h1>About</h1>
+    return <div>
+        <h1>About Anthony</h1>
+        <p>Anthony is an aspiring web developer from New Jersey who is studying at Rutgers, currently working as an equipment technician for a golf course. </p>
+        <p>Some skills I have learned during boot camp:</p>
+        <ul className="skillList">
+            <li>HTML/CSS</li>
+            <li>JavaScript</li>
+            <li>APIs</li>
+            <li>NodeJS</li>
+            <li>OOP</li>
+            <li>Express</li>
+            <li>SQL</li>
+            <li>ORM</li>
+            <li>MVC</li>
+            <li>NoSQL</li>
+            <li>PWA</li>
+            <li>React</li>
+            <li>MERN</li>
+        </ul>
+        </div>
 }
--- src/components/pages/MyWork.js
+++ src/components/pages/MyWork.js
@@ -1,3 +1,32 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+
+
 export default function MyWork() {
-    return <h1>My Work</h1>
+    
+    return <div><h1>My Work</h1>
+  <div className="picContainer">
+   <figure className="workimg">
+    <a href="https://ahoff6.github.io/01-HTML-Git-CSS-Challenge/">
+        <img src={require("../assets/images/Horiseonpic.png")} class="myworkpics"></img>
+      </a> 
+      </figure>
+      <figure className="workimg"> 
+      <a href="https://golemancer-ctrl.github.io/project-wikiview/">
+        <img src={require("../assets/images/HrryPotterFanPage.png")} class="myworkpics"></img>
+      </a>
+    </figure>
+    <figure className="workimg"> 
+      <a href="https://wdiwd-app.herokuapp.com/">
+        <img src={require("../assets/images/WDIWDpic.png")} class="myworkpics"></img>
+      </a>
+    </figure>
+    <figure className="workimg"> 
+      <a href="https://whatever-marketplace.herokuapp.com/">
+        <img src={require("../assets/images/MarketPlacepic.png")} class="myworkpics"></img>
+      </a>
+    </figure>
+      
+  </div>
+  </div>
 }
