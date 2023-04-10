9c320772c3e268112b529dac9b950214cf414672
ManHoff <achoffman91@gmail.com>
2023-04-10 13:54:59 -0400

    added icon links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+
 function Footer() {
   return (
     <footer className="footerContainer">
-      <p>This is my footer!</p>
+      
+      <ul className="iconContainer">
+      <li className="social-icon"><a href="https://www.linkedin.com/in/anthony-hoffman-0396a8263/"> <i className="fa fa-linkedin"></i></a></li>
+      <li className="social-icon"><a href="https://github.com/aHoff6"> <i className="fa fa-github"></i></a></li>
+      </ul>
     </footer>
   );
 }
