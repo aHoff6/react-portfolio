888f869b3d6319fa3b402d2330551cb46e278545
ManHoff <achoffman91@gmail.com>
2023-06-02 14:49:05 -0400

    updated about section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,9 @@
 export default function About() {
   return (
     <section className="about-container">
-      <h1 className="title" id="about-title">About Me</h1>
+      <h1 className="title" id="about-title">
+        About Me
+      </h1>
       <div>
         <figure className="aside-container">
           <img
@@ -9,39 +11,38 @@ export default function About() {
             alt="aboutProfilepic1"
             className="aboutPic"
           />
-        
-        <p className="about-text">
-          Aspiring web developer who recently completed the Full-Stack Coding
-          Bootcamp at Rutgers University, equipped with a strong foundation in front-end and back-end
-          technologies such as HTML, CSS, JavaScript, React, and Node.js. With a
-          creative and analytical mindset, I enjoy solving challenges and
-          delivering visually appealing and user-friendly web experiences.
-          Collaboration, communication, and continuous learning are key aspects
-          of my work, and I am eager to contribute my skills to projects that
-          make a positive impact.
-        </p>
-        
-      
-      
-      <ul className="skillList">
-      <h2 className="skills-title">Topics learned:</h2>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>API Interactions</li>
-        <li>User Authentication</li>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>MySQL</li>
-        <li>PWA</li>
-        <li>React.js</li>
-        <li>MERN Stack</li>
-        <li>Git</li>
-        <li>AJAX</li>
-      </ul>
-      </figure>
+
+          <p className="about-text">
+            Aspiring web developer who recently completed the Full-Stack Coding
+            Bootcamp at Rutgers University, equipped with a strong foundation in
+            front-end and back-end technologies such as HTML, CSS, JavaScript,
+            React, and Node.js. With a creative and analytical mindset, I enjoy
+            the challenges that delivering a visually appealing and
+            user-friendly web experience brings. Currently working as an
+            Equipment Technician at a high end golf course, problem solving and
+            troubleshooting are practiced daily. Overcoming an abundance of
+            daunting challenges throughout my career gave me the confidence
+            needed to take on the bootcamp and focus on a new passion. 
+          </p>
+
+          <ul className="skillList">
+            <h2 className="skills-title">Topics learned:</h2>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>API Interactions</li>
+            <li>User Authentication</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>MySQL</li>
+            <li>PWA</li>
+            <li>React.js</li>
+            <li>MERN Stack</li>
+            <li>Git</li>
+            <li>AJAX</li>
+          </ul>
+        </figure>
       </div>
-    
     </section>
   );
 }
