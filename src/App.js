4f12abe5a2ebc6b75b7ec3a447d1144e66c7489e
ManHoff <achoffman91@gmail.com>
2024-01-15 14:20:24 -0500

    fixed loading issue

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/mywork" element={<MyWork />} />
+            <Route path="/react-portfolio" element={<Home />} />
           </Routes>
         </div>
           <Footer />
